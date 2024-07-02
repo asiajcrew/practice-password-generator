49b211899d6944f88c322944d05244d3f0715461
asiajcrew <asia_jcrew96@yahoo.com>
2024-07-02 20:03:15 +0100

    added screenshot of site

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,18 @@
 
 var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specialCharacters = ["!", "#", "$", "&", "*", "(", ")", "-", "_"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["!", "#", "$", "&", "*", "(", ")", "-", "_", "@", "^", "+", ".", "<", ">", "/", "'", "|", ":"];
 var masterListArray = [];
 var inputArray = [];
 
-// generatePassword = (random selection from uppercase/ lowercase/ numbers/ specialcharacters arrays).length = passwordLength
+function resetPassword() {
+  var password = "";
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
 
 var generatePassword = function() {
   // Select password length
@@ -67,3 +73,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
